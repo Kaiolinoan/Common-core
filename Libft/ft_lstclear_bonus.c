23c6d14b6e682a1c99a2ba2b90c0d6ffd564c194
Kaio <kaiolinoan@gmail.com>
2025-04-22 18:57:01 +0100

    lst clear add

--- Libft/ft_lstclear_bonus.c
+++ Libft/ft_lstclear_bonus.c
@@ -12,7 +12,19 @@
 
 #include "libft.h"
 
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*temp;
+	t_list	*next;
 
+	if (!lst || !*lst || !del)
+		return ;
+	temp = *lst;
+	while (temp)
+	{
+		next = temp->next;
+		ft_lstdelone(temp, (del));
+		temp = next;
+	}
+	*lst = NULL;
 }
